291d28190bea0f7791098dddaf0c1f5376cb69f9
Quang Chu <anhquang.chu@sjsu.edu>
2021-10-05 10:33:59 -0700

    Add first homework problem
    The answer contains a multiply_list() function that will take a list as parameter, multiply all the elements in it together, and return the result

---
+++ multiply.py
@@ -0,0 +1,27 @@
+def multiply_list(list):
+	"""
+	Multiple all elements in the list
+	
+	This function take in a list then loop through it. For each loop, the previous result will be used to multiply
+	with the next element in the list. The initial result is 1. After the calculation, the function will return
+	the result.
+
+	Parameters
+	----------
+	list[int[
+		the input list whose elements are to be multiplied
+	
+	Returns
+	-------
+	int
+		the quotation of all the elements in the list
+
+	Example:
+	-------
+	 multiply_list([1,2,3]) #return 6
+	"""
+	#To store the quotation result to use after each loop
+	result = 1
+	for number in list:
+		result = result * number
+	return result
